afc0bc1b4023be3c17fb1373074125d592745b27
James Halliday <mail@substack.net>
2011-05-16 01:18:40 -0700

    failing test for start column

--- test/wrap.js
+++ test/wrap.js
@@ -9,8 +9,21 @@ exports.stop80 = function () {
     var words = idleness.split(/\s+/);
     
     lines.forEach(function (line) {
-        assert.ok(line.length <= 80, 'line <= 80 columns');
-        var chunks = line.length ? line.split(/\s+/) : [];
+        assert.ok(line.length <= 80, 'line > 80 columns');
+        var chunks = line.match(/\S/) ? line.split(/\s+/) : [];
         assert.deepEqual(chunks, words.splice(0, chunks.length));
+    });
+};
+
+exports.start20stop60 = function () {
+    var lines = wordwrap(20, 100)(idleness).split(/\n/);
+    var words = idleness.split(/\s+/);
+    
+    lines.forEach(function (line) {
+console.log(line);
+        assert.ok(line.length <= 100, 'line > 100 columns');
+        var chunks = line.match(/\S/) ? line.split(/\s+/) : [];
+        assert.deepEqual(chunks, words.splice(0, chunks.length));
+        assert.deepEqual(line.slice(0, 20), new Array(20).join(' '));
     });
 };
