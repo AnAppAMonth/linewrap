d6c40ead4f504f71710b29a6ea8e0a1f7f03ebcd
Feng Qiu <feng@ban90.com>
2013-08-09 11:52:33 +0800

    Add tests to verify that we produce the same result as wordwrap

--- test/break.js
+++ test/break.js
@@ -1,30 +1,39 @@
 var assert = require('assert');
-var wordwrap = require('../');
+var wordwrap = require('wordwrap');
+var wordwrap2 = require('../');
 
 exports.hard = function () {
     var s = 'Assert from {"type":"equal","ok":false,"found":1,"wanted":2,'
         + '"stack":[],"id":"b7ddcd4c409de8799542a74d1a04689b",'
         + '"browser":"chrome/6.0"}'
     ;
+
     var s_ = wordwrap.hard(80)(s);
+        s_2 = wordwrap2.hard(80)(s);
+
+    assert.equal(s_, s_2);
+
+    var lines2 = s_2.split('\n');
+    assert.equal(lines2.length, 2);
+    assert.ok(lines2[0].length < 80);
+    assert.ok(lines2[1].length < 80);
     
-    var lines = s_.split('\n');
-    assert.equal(lines.length, 2);
-    assert.ok(lines[0].length < 80);
-    assert.ok(lines[1].length < 80);
-    
-    assert.equal(s, s_.replace(/\n/g, ''));
+    assert.equal(s, s_2.replace(/\n/g, ''));
 };
 
 exports.break = function () {
     var s = new Array(55+1).join('a');
+
     var s_ = wordwrap.hard(20)(s);
+        s_2 = wordwrap2.hard(20)(s);
+
+    assert.equal(s_, s_2);
+
+    var lines2 = s_2.split('\n');
+    assert.equal(lines2.length, 3);
+    assert.ok(lines2[0].length === 20);
+    assert.ok(lines2[1].length === 20);
+    assert.ok(lines2[2].length === 15);
     
-    var lines = s_.split('\n');
-    assert.equal(lines.length, 3);
-    assert.ok(lines[0].length === 20);
-    assert.ok(lines[1].length === 20);
-    assert.ok(lines[2].length === 15);
-    
-    assert.equal(s, s_.replace(/\n/g, ''));
+    assert.equal(s, s_2.replace(/\n/g, ''));
 };
--- test/wrap.js
+++ test/wrap.js
@@ -1,14 +1,19 @@
 var assert = require('assert');
-var wordwrap = require('../');
+var wordwrap = require('wordwrap');
+var wordwrap2 = require('../');
 
 var fs = require('fs');
 var idleness = fs.readFileSync(__dirname + '/idleness.txt', 'utf8');
 
 exports.stop80 = function () {
-    var lines = wordwrap(80)(idleness).split(/\n/);
+	var res = wordwrap(80)(idleness),
+	    res2 = wordwrap2(80)(idleness);
+
+	assert.equal(res, res2);
+
+    var lines2 = wordwrap2(80)(idleness).split(/\n/);
     var words = idleness.split(/\s+/);
-    
-    lines.forEach(function (line) {
+    lines2.forEach(function (line) {
         assert.ok(line.length <= 80, 'line > 80 columns');
         var chunks = line.match(/\S/) ? line.split(/\s+/) : [];
         assert.deepEqual(chunks, words.splice(0, chunks.length));
@@ -16,10 +21,14 @@ exports.stop80 = function () {
 };
 
 exports.start20stop60 = function () {
-    var lines = wordwrap(20, 100)(idleness).split(/\n/);
+	var res = wordwrap(20, 100)(idleness),
+	    res2 = wordwrap2(20, 100)(idleness);
+
+	assert.equal(res, res2);
+
+    var lines2 = wordwrap2(20, 100)(idleness).split(/\n/);
     var words = idleness.split(/\s+/);
-    
-    lines.forEach(function (line) {
+    lines2.forEach(function (line) {
         assert.ok(line.length <= 100, 'line > 100 columns');
         var chunks = line
             .split(/\s+/)
