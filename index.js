63b1d469922a27857f50604adf53f9618feea8a7
Feng Qiu <feng@ban90.com>
2013-08-11 11:43:17 +0800

    Support 4 different whitespace options

--- index.js
+++ index.js
@@ -18,11 +18,29 @@ var wordwrap = module.exports = function (start, stop, params) {
 
     if (!params) params = {};
     var mode = params.mode || 'soft';
+    // Availbalbe options: 'collapse', 'default', 'line', and 'all'
+    var whitespace = params.whitespace || 'default';
+
     var re = mode === 'hard' ? /\b/ : /(\S+\s+)/;
     var prefix = new Array(start + 1).join(' ');
+    var stripPrecedingWS = !(whitespace === 'line' || whitespace === 'all'),
+        stripTrailingWS = !(whitespace === 'all');
+    var pPat, tPat, last;
+
+    if (stripTrailingWS) {
+        tPat = new RegExp('^( {' + start + '}.*?)\\s+$');
+    } else {
+        tPat = new RegExp('^(.{' + stop + '}.*?)\\s+$');
+    }
 
     return function (text) {
-        var chunks = text.toString()
+		text = text.toString().replace(/\t/g, '    ');
+
+		if (whitespace === 'collapse') {
+			text = text.replace(/  +/g, ' ');
+		}
+
+        var chunks = text
             .split(re)
             .reduce(function (acc, x) {
                 if (mode === 'hard') {
@@ -32,13 +50,11 @@ var wordwrap = module.exports = function (start, stop, params) {
                 }
                 else acc.push(x)
                 return acc;
-            }, [])
-        ;
+            }, []);
 
-        chunks = chunks.reduce(function (lines, rawChunk) {
-            if (rawChunk === '') return lines;
+        chunks = chunks.reduce(function (lines, chunk) {
+            if (chunk === '') return lines;
 
-            var chunk = rawChunk.replace(/\t/g, '    ');
             var i = lines.length - 1;
             var xs = chunk.split(/\n/),
                 curr = xs[0];
@@ -50,19 +66,27 @@ var wordwrap = module.exports = function (start, stop, params) {
             }
 
             xs.forEach(function (c) {
-                if (i === lines.length -1 ) {
-                    lines[i] = lines[i].replace(/\s+$/, '');
-                    if (!lines[i]) lines[i] = prefix;
+                last = lines.length - 1;
+                if (stripTrailingWS || lines[last].length > stop) {
+                    lines[last] = lines[last].replace(tPat, '$1');
                 }
-                lines.push(prefix + c.replace(/^\s+/, ''));
+                if (stripPrecedingWS) {
+                    c = c.replace(/^\s+/, '');
+                }
+                lines.push(prefix + c);
             });
 
             return lines;
         }, [ prefix ]);
 
-        var last = chunks.length - 1;
-        chunks[last] = chunks[last].replace(/\s+$/, '');
-        if (!chunks[last]) chunks[last] = prefix;
+        if (stripPrecedingWS) {
+            pPat = new RegExp('^( {' + start + '})\\s+(.*)$');
+            chunks[0] = chunks[0].replace(pPat, '$1$2');
+        }
+        last = chunks.length - 1;
+        if (stripTrailingWS || chunks[last].length > stop) {
+            chunks[last] = chunks[last].replace(tPat, '$1');
+        }
         return chunks.join('\n');
     };
 };
