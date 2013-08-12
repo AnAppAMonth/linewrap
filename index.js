febf1367431a419565326e7b7b483c93a6a549c4
Feng Qiu <feng@ban90.com>
2013-08-12 09:43:46 +0800

    Add support for skip sequences

--- index.js
+++ index.js
@@ -22,11 +22,42 @@ var wordwrap = module.exports = function (start, stop, params) {
     var whitespace = params.whitespace || 'default';
     var tabWidth = params.tabWidth || 4;
 
+    // Precedence: Scheme > Regex > Str
+    var skipScheme = params.skipScheme,
+        skipPat = params.skipRegex;
+    if (skipScheme) {
+        // Supported schemes: 'color', 'html', 'bbcode'
+        if (skipScheme === 'color') {
+            skipPat = /\x1B\[[^m]*m/g;
+        } else if (skipScheme === 'html') {
+            skipPat = /<[^>]*>/g;
+        } else if (skipScheme === 'bbcode') {
+            skipPat = /\[[^]]*\]/g;
+        }
+    }
+    if (skipPat instanceof RegExp) {
+        if (!skipPat.global) {
+            var flags = 'g';
+            if (skipPat.ignoreCase) {
+                flags += 'i';
+            } else if (skipPat.multiline) {
+                flags += 'm';
+            }
+            skipPat = new RegExp(skipPat.source, flags);
+        } else {
+            skipPat.lastIndex = 0;
+        }
+    } else if (params.skipStr) {
+        skipPat = new RegExp(params.skipStr, 'g');
+    } else {
+        skipPat = undefined;
+    }
+
     var re = mode === 'hard' ? /\b/ : /(\S+\s+)/;
     var prefix = new Array(start + 1).join(' ');
     var stripPrecedingWS = !(whitespace === 'line' || whitespace === 'all'),
         stripTrailingWS = !(whitespace === 'all');
-    var pPat, tPat, last;
+    var pPat, tPat;
 
     if (stripTrailingWS) {
         tPat = new RegExp('^( {' + start + '}.*?)\\s+$');
@@ -41,40 +72,79 @@ var wordwrap = module.exports = function (start, stop, params) {
 			text = text.replace(/  +/g, ' ');
 		}
 
-        var chunks = text
-            .split(re)
-            .reduce(function (acc, x) {
-                if (mode === 'hard') {
-                    for (var i = 0; i < x.length; i += stop - start) {
-                        acc.push(x.slice(i, i + stop - start));
-                    }
-                }
-                else acc.push(x)
-                return acc;
-            }, []);
+        var segments, match, base = 0;
+        if (skipPat) {
+            segments = [];
+            match = skipPat.exec(text);
+            while(match) {
+                segments.push(text.substring(base, match.index));
+                segments.push(new String(match[0]));
+                base = match.index + match[0].length;
+                match = skipPat.exec(text);
+            }
+            segments.push(text.substring(base));
+        } else {
+            segments = [text];
+        }
 
+        var chunks = segments.reduce(function (result, segment) {
+            if (typeof segment === 'string') {
+                var parts = segment
+                    .split(re)
+                    .reduce(function (acc, x) {
+                        if (mode === 'hard') {
+                            for (var i = 0; i < x.length; i += stop - start) {
+                                acc.push(x.slice(i, i + stop - start));
+                            }
+                        }
+                        else acc.push(x);
+                        return acc;
+                    }, []);
+                return result.concat(parts);
+            } else {
+                result.push(segment);
+                return result;
+            }
+        }, []);
+
+        var curLine = 0,
+            curLineLength = start;
         chunks = chunks.reduce(function (lines, chunk) {
             if (chunk === '') return lines;
 
-            var i = lines.length - 1;
-            var xs = chunk.split(/\n/),
-                curr = xs[0];
+            if (typeof chunk !== 'string') {
+                // Assumption: skip strings don't end with whitespaces.
+                if (curLineLength >= stop) {
+                    if (stripTrailingWS || curLineLength > stop) {
+                        lines[curLine] = lines[curLine].replace(tPat, '$1');
+                    }
+                }
+                lines[curLine] += chunk;
+                return lines;
+            }
 
-            if (!(lines[i].length + curr.length > stop &&
-                    lines[i].length + curr.replace(/\s+$/, '').length > stop &&
-                    lines[i].length > start)) {
-                lines[i] += xs.shift();
+            var xs = chunk.split(/\n/),
+                curr = xs[0],
+                curr2;
+
+            if (!(curLineLength + curr.length > stop &&
+                    curLineLength + (curr2 = curr.replace(/\s+$/, '')).length > stop &&
+                    curr2 !== '' &&
+                    curLineLength > start)) {
+                lines[curLine] += xs.shift();
+                curLineLength += curr.length;
             }
 
             xs.forEach(function (c) {
-                last = lines.length - 1;
-                if (stripTrailingWS || lines[last].length > stop) {
-                    lines[last] = lines[last].replace(tPat, '$1');
+                if (stripTrailingWS || curLineLength > stop) {
+                    lines[curLine] = lines[curLine].replace(tPat, '$1');
                 }
                 if (stripPrecedingWS) {
                     c = c.replace(/^\s+/, '');
                 }
                 lines.push(prefix + c);
+                curLine++;
+                curLineLength = start + c.length;
             });
 
             return lines;
@@ -84,9 +154,8 @@ var wordwrap = module.exports = function (start, stop, params) {
             pPat = new RegExp('^( {' + start + '})\\s+(.*)$');
             chunks[0] = chunks[0].replace(pPat, '$1$2');
         }
-        last = chunks.length - 1;
-        if (stripTrailingWS || chunks[last].length > stop) {
-            chunks[last] = chunks[last].replace(tPat, '$1');
+        if (stripTrailingWS || curLineLength > stop) {
+            chunks[curLine] = chunks[curLine].replace(tPat, '$1');
         }
         return chunks.join('\n');
     };
